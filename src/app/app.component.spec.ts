ebf626d2c7df1eb12f299429b669beb40d94f442
Ansen123 <ansenfp@gmail.com>
2022-10-07 13:32:20 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'employe'`, () => {
+  it(`should have as title 'Employe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('employe');
+    expect(app.title).toEqual('Employe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('employe app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Employe app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddemployeComponent } from './addemploye/addemploye.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddemployeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
